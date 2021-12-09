52a6c217d2889a48c377c486172d9c3c9ed68738
docker-knzilewis <knzilewis@studenten.hs-bremerhaven.de>
2021-12-09 18:44:37 +0100

    updating new stand

--- tools/Head.java
+++
@@ -1,7 +0,0 @@
-import test.*;
-public class Head{
-  public static void main(String[] args){
-    System.out.println("Test main");
-    Worker.myworker();
-  }
-}
--- tools/src/test/Worker.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-public class Worker {
-  public static void myworker() {
-    System.out.println("ich mache etwas: myworker");
-  }
-}
